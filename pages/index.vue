e734855588a56f205bdce8ca903226f6c184dc04
soareslssa <soareslssa.dev@gmail.com>
2024-03-15 03:33:15 -0300

    feat: add nuxtLayout

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="d-flex flex-column pa-4 ga-4 justify-center mt-16 mt-md-0">
+    <span class="text-h4 text-center">Find your drive</span>
+    <v-text-field  label="Label" variant="solo"></v-text-field>
+  </div>
+</template>
+<script setup>
+</script>
