fc13df779cb25739f2f731018f96835fa763b6e1
soareslssa <soareslssa.dev@gmail.com>
2024-03-15 16:55:44 -0300

    feat: implement layout and index

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <v-toolbar title="cardtrader"></v-toolbar>
+    <v-toolbar title="cardtrader" color="white" density="compact"></v-toolbar>
     <slot />
   </div>
 </template>
-<style >
-body {
-  background-image: url('https://images.unsplash.com/photo-1503376780353-7e6692767b70?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="d-flex flex-column pa-4 ga-4 justify-center mt-16 mt-md-0">
-    <span class="text-h4 text-center">Find your drive</span>
-    <v-text-field  label="Label" variant="solo"></v-text-field>
+  <div class="d-flex align-center h-screen pa-6 body">
+    <div class="d-flex flex-column w-100 mb-16 ga-4 align-center">
+      <span class="text-h4 text-center text-white">Find your drive</span>
+      <v-text-field
+        class="responsive-text-field"
+        label="find something..."
+        variant="solo"
+      ></v-text-field>
+    </div>
   </div>
 </template>
 <script setup>
 </script>
+<style scoped>
+.body {
+  background-image: url("https://images.unsplash.com/photo-1542282088-72c9c27ed0cd?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  background-size: cover;
+}
+.responsive-text-field {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .responsive-text-field {
+    width: 50%;
+  }
+}
+</style>
