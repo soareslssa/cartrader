5aed7d10d80a5de96389832d0b42ede377448ea3
soareslss <soareslssa@fab.mil.br>
2024-03-20 13:18:53 -0300

    feat: add CarDetails components

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+  >
+    <div>
+      <CarDetailHero />
+    </div>
+
+    <CarDetailAttributes />
+
+    <CarDetailDescription />
+  </div>
+</template>
