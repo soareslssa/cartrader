29bad28a563a520a1ca71823b538026804c74072
soareslss <soareslssa@fab.mil.br>
2024-03-20 15:50:25 -0300

    feat: add custom layout

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -5,17 +5,17 @@ const { toTitleCase } = useString();
 useHead({
   title: `${toTitleCase(route.params.name)}`,
 });
+
+definePageMeta({
+  layout: "custom",
+});
 </script>
 <template>
-  <div
-    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
-  >
-    <div>
-      <CarDetailHero />
-    </div>
-
-    <CarDetailAttributes />
-
-    <CarDetailDescription />
+  <div>
+    <CarDetailHero />
   </div>
+
+  <CarDetailAttributes />
+
+  <CarDetailDescription />
 </template>
