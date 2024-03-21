3a3fadae55f37731be8121803c90f7af7f0f4326
soareslss <soareslssa@fab.mil.br>
2024-03-21 10:56:45 -0300

    feat: add responsive car hero layout

--- components/Car/Hero.vue
+++ components/Car/Hero.vue
@@ -4,8 +4,10 @@
     class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://carconfigurator.ferrari.com/assets/cars/portofinom/packages/default/car-ferrari-portofino-m_splash.jpg')]"
   >
     <div class="z-10 absolute w-full h-full flex justify-center items-center">
-      <div class="text-center w-1/2">
-        <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+      <div class="text-center w-100">
+        <h1 class="sm:text-8xl text-4xl font-serif mb-10 text-white">
+          Find your drive
+        </h1>
         <CarSearchBar />
       </div>
     </div>
--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -4,7 +4,7 @@ const cityError = ref<boolean>(false);
 
 const handleSearch = () => {
   if (!city.value) {
-    return cityError.value = true;
+    return (cityError.value = true);
   }
 
   navigateTo(`/city/${city.value}/car`);
@@ -12,18 +12,20 @@ const handleSearch = () => {
 </script>
 <template>
   <div
-    class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
+    class="font-serif md:w-1/2 w-80 text-sm md:text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
   >
     <input
       type="text"
-      class="py-3 px-5 w-full border-4 border-red-500 text-2xl rounded-full focus:outline-none"
+      class="py-3 px-5 w-full text-sm md:text-2xl rounded-full focus:outline-none"
       placeholder="Search by city..."
-      :class="cityError ? 'border-2 border-red-500': ''"
+      :class="cityError ? 'border-2 border-red-500' : ''"
       v-model="city"
     />
     <button class="bg-sky-500 px-10 text-white" @click="handleSearch">
       Search
     </button>
-    <v-snackbar v-model="cityError" color="warning">Campo necessário</v-snackbar>
+    <v-snackbar v-model="cityError" color="warning"
+      >Campo necessário</v-snackbar
+    >
   </div>
 </template>
