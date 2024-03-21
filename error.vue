8b83b7abe10c71ea3cea45deacf3b4b44735eb22
soareslss <soareslssa@fab.mil.br>
2024-03-21 13:27:44 -0300

    feat: dynamic error useError composable

--- error.vue
+++ error.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-6xl sm:text-9xl">404</h1>
-    <p class="my-5 text-4xl">Page not found</p>
+    <h1 class="text-6xl sm:text-9xl">{{error.statusCode}}</h1>
+    <p class="my-5 text-4xl">{{ error.message}}</p>
     <v-btn @click="handleReturn" flat small color="primary">Go back</v-btn>
   </div>
 </template>
 
 <script setup lang="ts">
+
+const error = useError();
+
 const handleReturn = () => {
   navigateTo("/");
 };
