e1eb3f32ded31080b1323c07e0eefc0bbaeeb9b3
soareslss <soareslssa@fab.mil.br>
2024-04-16 14:45:18 -0300

    feat: implement favored button

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,22 +1,41 @@
 <script setup lang="ts">
+import heartOutline from "@/assets/images/heartOutline.png";
+import heartFilled from "@/assets/images/heartFilled.png";
+
 const props = defineProps({
   car: Object,
 });
+
+const favored = useState(`favored-${props.car.id}`, () => {
+  return false;
+});
 </script>
 <template>
-  <div
-    @click="navigateTo(`/car/${car.name}-${car.id}`)"
-    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-full"
-  >
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-full">
     <div class="flex flex-col md:flex-row h-full">
-      <NuxtImg :src="car.url" alt="" class="w-full md:w-[300px] h-full" />
+      <NuxtImg
+        :src="car.url"
+        alt=""
+        class="w-full md:w-[300px] h-full"
+        @click="navigateTo(`/car/${car.name}-${car.id}`)"
+      />
       <div class="p-4 flex flex-col">
-        <div>
-          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
-          <p class="text-gray-700">
-            {{ car.description }}
-          </p>
+        <div class="w-full flex flex-wrap justify-between">
+          <div class="flex flex-grow">
+            <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+          </div>
+          <div>
+            <img
+              class="size-6"
+              :src="favored ? heartFilled : heartOutline"
+              @click="favored = !favored"
+            />
+          </div>
         </div>
+        <p class="text-gray-700">
+          {{ car.description }}
+        </p>
+
         <h1 class="mt-auto text-xl">${{ car.price }}</h1>
       </div>
     </div>
